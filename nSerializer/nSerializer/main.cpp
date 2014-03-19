765d5f93bd9af1bdecf2301fd09d2192412d86fb
geofl <GEO@sns71-127.york.ac.uk>
2014-03-19 18:37:18 +0000

    Initial Commit

---
+++ nSerializer/nSerializer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nSerializer
+//
+//  Created by GEO on 19/03/2014.
+//  Copyright (c) 2014 GEO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
